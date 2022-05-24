0452a93db6a29e187e416b7cfda710ec8007a0d8
Hamza <ansarihamza438@gmail.com>
2022-05-24 12:43:02 +0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/outkashd1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
