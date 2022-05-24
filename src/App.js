80e6ffdc24933f47c5d009264c6d98bc6fedb71b
Hamza <ansarihamza438@gmail.com>
2022-05-24 12:31:39 +0500

    modified App.js to say Sefyul Kashem

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Name is Seyful Kashem and I am going to master React
         </p>
         <a
           className="App-link"
